a1f0468e69f6affa5716f4259510b95ba64723f2
mihailradulov <mihail.radulov@gmail.com>
2023-03-09 12:19:04 +0200

    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 
 streamlit.title('My parrents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Datmeal')
+streamlit.text('Kale, Spinache & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Ranged Egg')
