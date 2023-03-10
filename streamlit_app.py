72391021ca67c0a42711e89a14dacf8d246a11f5
mihailradulov <mihail.radulov@gmail.com>
2023-03-10 13:59:36 +0200

    first function introduced

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice API response
+def get_fruityvice_data(this_fruit_choice)
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     return fruityvice_normalized
+
 streamlit.header('FruityVice Fruite Advice!')
 
 try:
@@ -33,15 +38,11 @@ try:
     if not fruit_choice:
         streamlit.error('Please select a fruit to get information.')
     else:
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-            # Takes JSON response and normalize it and dataframe it
-            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-            streamlit.dataframe(fruityvice_normalized)
+            
+            streamlit.dataframe(get_fruityvice_data(fruit_choice))
     streamlit.write('The user entered ', fruit_choice)
 except URLError as e:
     streamlit.error
-
-
 
 streamlit.stop()
 # Open cursor with snowflake data
